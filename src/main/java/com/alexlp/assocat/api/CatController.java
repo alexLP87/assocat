a97bc157618f1ea51d30b1039192e09590f51506
alexLP <alexis.lepretre@gmail.com>
2020-12-03 14:15:25 +0100

    cat controller implementation OK

--- src/main/java/com/alexlp/assocat/api/CatController.java
+++ src/main/java/com/alexlp/assocat/api/CatController.java
@@ -1,9 +1,15 @@
 package com.alexlp.assocat.api;
 
+import com.alexlp.assocat.model.Cat;
 import com.alexlp.assocat.service.CatService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RequestMapping("api/v1/cat")
 @RestController
@@ -16,5 +22,30 @@ public class CatController {
         this.catService = catService;
     }
 
-    //TODO implements cat get, post etc...request
+
+
+    @GetMapping
+    public List<Cat> getAllCats(){
+        return catService.getAllCats();
+    }
+
+    @GetMapping(path = "{id}")
+    Optional<Cat> getCatById(@PathVariable("id") UUID id){
+        return catService.getCatById(id);
+    }
+
+    @PostMapping
+    public void addCat(@Valid @NotNull @RequestBody Cat cat){
+        catService.addCat(cat);
+    }
+
+    @PutMapping(path = "{id}")
+    public void updateCat(@PathVariable("id") UUID id, @Valid @NotNull @RequestBody Cat catToUpdate){
+        catService.updateCatById(id, catToUpdate);
+    }
+
+    @DeleteMapping(path = "{id}")
+    public void deleteCatById(@PathVariable("id") UUID id){
+        catService.deleteCatById(id);
+    }
 }

14bf1bf109466be83557f2b3614c0e51d1b99e32
alexLP <alexis.lepretre@gmail.Com>
2020-12-08 14:54:25 +0100

    hibernate conf and bean config, spring running

--- src/main/java/com/alexlp/assocat/dao/UserDataAccessService.java
+++ src/main/java/com/alexlp/assocat/dao/UserDataAccessService.java
@@ -1,6 +1,8 @@
 package com.alexlp.assocat.dao;
 
 import com.alexlp.assocat.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
@@ -11,7 +13,7 @@ import java.util.UUID;
 
 @Repository("postgreuser")
 public class UserDataAccessService implements UserDao{
-
+/*
     //inject jdbc connector
     private final JdbcTemplate jdbcTemplate;
 
@@ -19,6 +21,9 @@ public class UserDataAccessService implements UserDao{
     public UserDataAccessService(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
+*/
+    @Autowired
+    private SessionFactory sessionFactory;
 
     //TODO implement database query UserDataService
     @Override
@@ -33,7 +38,21 @@ public class UserDataAccessService implements UserDao{
 
     @Override
     public Optional<User> selectUserById(UUID id) {
-        return Optional.empty();
+        Session session = sessionFactory.getCurrentSession();
+        User user;
+
+
+        try {
+            session.beginTransaction();
+            user = session.get(User.class, "45214688-3639-11eb-923b-0242ac130002");
+
+            session.getTransaction().commit();
+        } finally {
+            session.close();
+        }
+
+        Optional<User> retour = Optional.ofNullable(user);
+        return retour;
     }
 
     @Override
--- src/main/java/com/alexlp/assocat/model/User.java
+++ src/main/java/com/alexlp/assocat/model/User.java
@@ -3,22 +3,39 @@ package com.alexlp.assocat.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import java.util.UUID;
 
+@Entity
+@Table(name = "users")
 public class User {
 
-    private final UUID id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private UUID id;
 
+    @Column(name = "name")
     @NotBlank
-    private final String name;
-    @NotBlank
-    private final String firstName;
+    private String name;
 
+    @Column(name = "firstname")
+    @NotBlank
+    private String firstName;
+
+    /*
     public User(@JsonProperty("id") UUID id, @JsonProperty("name") String name, @JsonProperty("firstname") String firstName) {
         this.id = id;
         this.name = name;
         this.firstName = firstName;
+    }
+
+     */
+
+    public User() {
+
     }
 
     public UUID getId() {
@@ -31,5 +48,17 @@ public class User {
 
     public String getFirstName() {
         return firstName;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 }
